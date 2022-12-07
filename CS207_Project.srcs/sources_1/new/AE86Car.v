5ee5413c65e2e178157f4f304b8bbf5bfd205e4b
seth <22097951+Seth2482@users.noreply.github.com>
2022-12-07 19:40:44 +0800

    定义输入和输出 项目入口文件改名

--- CS207_Project.srcs/sources_1/new/AE86Car.v
+++ CS207_Project.srcs/sources_1/new/AE86Car.v
@@ -21,13 +21,42 @@
 
 
 module GlobalState(
-    input power_on,power_off,clk,
-    input [1:0] mode_selection
-
+    input clk,           
+    input power_on,             // 点火 button
+    input power_off,            // 熄火 button
+    input throttle,             // 油门 switch 
+    input clutch,               // 离合 switch
+    input break,                // 刹车 switch
+    input reverse_gear,         // 倒车 switch
+    input turn_left,            // 左转 switch
+    input turn_right,           // 右转 switch
+    input rx,                   // 输入 绑定到 N5
+    output tx,                  // 输出 绑定到 T4
+    input [1:0] mode_selection, // 模式选择 switch*2
+    output turn_left_light,     // 左转灯 led
+    output turn_right_light,    // 右转灯 led
+    output [7:0] seg_en,        // 8 个流水灯开关
+    output [7:0] seg_out0,      // 前 4 个流水灯输出
+    output [7:0] seg_out1       // 后 4 个流水灯输出
     );
 
-parameter On =1'b1,Off=1'b0 ;
+// Global States
 reg power_state;//电源状态
+
+//各个模式的输出，绑定在simulate的输入
+reg turn_left_signal;
+reg turn_right_signal;
+reg move_forward_signal;
+reg move_backward_signal;
+reg place_barrier_signal;
+reg destroy_barrier_signal;
+wire front_detector;
+wire back_detector;
+wire left_detector;
+wire right_detector;
+
+parameter On =1'b1,Off=1'b0 ;
+
 always @(posedge power_off) begin
     power_state<=Off;
 end
@@ -53,22 +82,6 @@ end
 
 MannualDriving();
 
-
-//各个模式的输出，绑定在simulate的输入
-reg turn_left_signal;
-reg turn_right_signal;
-reg move_forward_signal;
-reg move_backward_signal;
-reg place_barrier_signal;
-reg destroy_barrier_signal;
-
-reg rx;
-
-wire tx;
-wire front_detector;
-wire back_detector;
-wire left_detector;
-wire right_detector;
 SimulatedDevice simulate(
     clk,
     rx,
